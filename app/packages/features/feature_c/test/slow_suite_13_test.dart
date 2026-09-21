22452713ff998eeb1932e9eaddf6180f1f9fd3b5
Dennis Kugelmann <dennis.kugelmann@simpleclub.com>
2026-09-21 18:25:37 +0000

    test: update feature c fixture

--- app/packages/features/feature_c/test/slow_suite_13_test.dart
+++ app/packages/features/feature_c/test/slow_suite_13_test.dart
@@ -1,12 +1,13 @@
 @Timeout(Duration(seconds: 420))
 import 'dart:async';
 
+import 'package:feature_c/feature_c.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('slow fake suite 13', () async {
     await Future<void>.delayed(const Duration(seconds: 360));
-    expect(13 > 0, isTrue);
+    expect(featureCValue(), contains('feature_c_updated'));
   });
 
   test('slow fake suite 13 - second assertion', () async {
